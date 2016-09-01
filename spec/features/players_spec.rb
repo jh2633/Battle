d9aa9e2fddd4f678a9c231b93b58ed0f60d2d465
mrmurtz <murtaza.s.abidi@gmail.com>
2016-09-01 15:03:42 +0100

    ch22 complete, player 1 can attack, tests passing

--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature 'Entering name of players' do
 
   scenario 'player 1 & 2 enter names' do
-    visit("/")
-    fill_in("player1", with: "Jon")
-    fill_in("player2", with: "Mertz")
-    click_button("submit")
+    sign_in_and_play
     expect(page).to have_content("Jon vs. Mertz")
   end
 end
