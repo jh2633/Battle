53fff1523ffe4dd1a325b4a4f412bb575e268dc5
mrmurtz <murtaza.s.abidi@gmail.com>
2016-09-02 12:46:59 +0100

    Added players turn to indicate whos turn it is

--- spec/features/switch_spec.rb
+++ spec/features/switch_spec.rb
@@ -9,4 +9,15 @@ feature 'switch' do
     expect(page).to have_content("Jon: 50HP")
   end
 
+  scenario 'player 1 has first turn ' do
+    sign_in_and_play
+    expect(page).to have_content("Jon's turn")
+  end
+
+  scenario 'display player 2 turn after switch' do
+    sign_in_and_play
+    click_link "Attack"
+    click_link "Switch"
+    expect(page).to have_content("Murtz's turn")
+  end
 end
